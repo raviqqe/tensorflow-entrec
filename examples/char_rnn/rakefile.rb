0601df7909fdacb31ba0b9d3ac6134bd632c24ba
Yota Toyama <raviqqe@gmail.com>
2017-05-25 18:58:26 +0900

    Fix example setting

--- examples/char_rnn/rakefile.rb
+++ examples/char_rnn/rakefile.rb
@@ -1,11 +1,12 @@
 task :default do
-  sh %W[python main.py
+  sh %w[python main.py
         --num_classes 2
         --char_file var/chars.txt
-        --train_file var/train/\*.json
-        --eval_file var/eval/\*.json
+        --train_file var/train/\\*.json
+        --eval_file var/eval/\\*.json
         --train_steps 20
-        --eval_steps 20
-        --batch_size 4
+        --eval_steps 1
+        --batch_size 1
+        --batch_queue_capacity 1
         --char_space_size 128].join(' ')
 end
